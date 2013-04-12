7886f49a74dd9f6c2c660dc306e834c297759568
dmoore5050 <dustinmoore@gmail.com>
2013-04-12 09:09:46 -0500

    koans progress: day 3

--- about_regular_expressions.rb
+++ about_regular_expressions.rb
@@ -156,6 +156,6 @@ class AboutRegularExpressions < EdgeCase::Koan
   end
 
   def test_gsub_is_like_find_and_replace_all
-    assert_equal "one t-t", "one two-three".gsub(/(t\w*)/) { $1[0, 1] }
+    assert_equal "one two-thr", "one two-three".gsub(/(t\w*)/) { $1[0, 3] }
   end
 end
